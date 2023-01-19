6de92c0cd2861795d437c4852f2f7ba247fcebdc
gangdonguri <rhkdgnsvk2@gmail.com>
2023-01-19 05:29:47 +0000

    [UPDATE] mongodb connection && root.js && index.js v1.4

--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -2,15 +2,16 @@
 
 const fp = require('fastify-plugin')
 
-const mongodb = require('@fastify/mongodb')
+const { MONGO_HOSTNAME, MONGO_USERNAME, MONGO_PASSWORD } = process.env
 
 module.exports = fp(async function (fastify, opts) {
-  console.log("MONGO_URL:", process.env.MONGO_URL);
-  
-  fastify.register(mongodb, {
+  const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:27017/?authMechanism=DEFAULT`
+  console.log(url)
+
+  fastify.register(require('@fastify/mongodb'), {
     forceClose: true,
-    url: process.env.MONGO_URL
-  } )
-  
-  console.log('mongobd 플러그인 등록 성공')
+    url: url
+  })
+
+  console.log('플러그인 등록에 성공하였습니다.')
 })
--- routes/root.js
+++ routes/root.js
@@ -1,41 +1,9 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
-    const db = fastify.mongo.client.db('TEST')
-
-    fastify.get('/', async function (req, reply) {
-        const result = await db.collection('text').find().toArray()
-
-        if(result.length !== 0) {
-            reply
-                .code(200)
-                .header('Content-type', 'Application/json')     
-                .send(result)
-        } else {
-            reply
-                .code(404)
-                .header('Content-type', 'plain/text')
-                .send('Not Found')
-        }
-    })
-
-    fastify.post('/', async function(req, reply) {
-        const result = await db.collection('text').insertOne(req.body)
-
-        if(result) {
-            reply
-                .code(201)
-                .header('Content-type', 'Application/json')     
-                .send({
-                    message: 'complete to write',
-                    title: req.body.title,
-                    content: req.body.content
-                })
-        } else {
-            reply
-                .code(400)
-                .header('Content-type', 'plain/text')
-                .send('Bad Request')
-        }
-    })
+  fastify.get('/', async function (request, reply) {
+    console.log(process.env.MONGO_HOSTNAME)
+    console.log(process.env.MONGO_USERNAME)
+    return { root: true, host: process.env.MONGO_HOSTNAME, username: process.env.MONGO_USERNAME }
+  })
 }
