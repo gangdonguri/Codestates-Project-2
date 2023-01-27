191ce30aaccb068eec952cf33092939360ba15e3
gangdonguri <rhkdgnsvk2@gmail.com>
2023-01-27 06:31:37 +0000

    [MODIFIED && ADD] 매장목록 조회, 모든 주문내역 조회 코드 변경 및 개별 주문확인 기능 추가

--- routes/api/orders/index.js
+++ routes/api/orders/index.js
@@ -2,15 +2,112 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (req, reply) {
-    const baedal = this.mongo.client.db('baedal');
-    const restaurant = baedal.collection('order');
-    const result = await restaurant.find({}).toArray();
+    console.log('-------------------------------------------------------------------')
+    const db = this.mongo.client.db('baedal');
+    const collectionOfOrders = db.collection('order');
+    const collectionOfrestaurants = db.collection('restaurants');
+    const orders = await collectionOfOrders.find({}).toArray();
+    const restaurants = await collectionOfrestaurants.find({}).toArray();
+
+    let rating = 0;
+    let restaurantId;
+    let restaurantMenu = [];
+    let currentOrderedMenu = [];
+    let result = [];
+
+    for (let order of orders) {
+      for (let restaurant of restaurants) {
+        if (order.restaurant.name === restaurant.name) {
+          restaurantId = restaurant._id;
+          for (let i = 0; i < restaurant.menu.length; i++) {
+            restaurantMenu.push(restaurant.menu[i])
+          }
+        }
+      }
+
+      for (let i = 0; i < order.orderedMenu.length; i++) {
+        currentOrderedMenu.push(order.orderedMenu[i])
+      }
+      
+      result.push(
+        {
+          "_id": `${order._id}`,
+          "restaurant": {
+            "_id": `${restaurantId}`,
+            "name": `${order.restaurant.name}`,
+            "menu": restaurantMenu,
+            "address": `${order.restaurant.address}`,
+            "rating": rating,
+          },
+          "orderedMenu": currentOrderedMenu,
+          "deliveryInfo": order.deliveryInfo
+        }
+      )
+      restaurantMenu = [];
+      currentOrderedMenu = [];
+      rating += 1
+    }
 
     reply
       .code(200)
       .header('content-type', 'application/json')
       .send(result)
     
+  })
+
+  fastify.get('/:id', async function (req, reply) {
+    const db = this.mongo.client.db('baedal');
+    const collectionOfOrders = db.collection('order');
+    const collectionOfrestaurants = db.collection('restaurants');
+    const orders = await collectionOfOrders.find({}).toArray();
+    const restaurants = await collectionOfrestaurants.find({}).toArray();
+    
+    let currentOrder;
+    let restaurantId;
+    let restaurantMenu = [];
+    let rating;
+    let index = 0;
+    let currentOrderedMenu = [];
+
+    for (let order of orders) {
+      if (req.params.id === `${order._id}`) {
+        currentOrder = order;
+        rating = index;
+      }
+      index += 1;
+    }
+
+    for (let restaurant of restaurants) {
+      if (currentOrder.restaurant.name === restaurant.name) {
+        restaurantId = restaurant._id;
+        for (let i = 0; i < restaurant.menu.length; i++) {
+          restaurantMenu.push(restaurant.menu[i])
+        }
+      }
+    }
+
+    for (let i = 0; i < currentOrder.orderedMenu.length; i++) {
+      currentOrderedMenu.push(currentOrder.orderedMenu[i])
+    }
+
+    const result = {
+      "_id": `${currentOrder._id}`,
+      "restaurant": {
+        "_id": `${restaurantId}`,
+        "name": `${currentOrder.restaurant.name}`,
+        "menu": restaurantMenu,
+        "address": `${currentOrder.restaurant.address}`,
+        "rating": rating
+      },
+      "orderedMenu": currentOrderedMenu,
+      "deliveryInfo": currentOrder.deliveryInfo
+    }
+
+    reply
+      .code(200)
+      .header('content-type', 'application/json')
+      .send(result)
+
   })
 }
 
