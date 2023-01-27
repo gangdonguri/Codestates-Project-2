e729d21e744cb9c9d449373793836e1e28e200a7
gangdonguri <rhkdgnsvk2@gmail.com>
2023-01-27 08:38:13 +0000

    [ADD] 주문 전송 기능 추가

--- routes/api/orders/index.js
+++ routes/api/orders/index.js
@@ -9,7 +9,7 @@ module.exports = async function (fastify, opts) {
     const orders = await collectionOfOrders.find({}).toArray();
     const restaurants = await collectionOfrestaurants.find({}).toArray();
 
-    let rating = 0;
+    let rating;
     let restaurantId;
     let restaurantMenu = [];
     let currentOrderedMenu = [];
@@ -19,6 +19,7 @@ module.exports = async function (fastify, opts) {
       for (let restaurant of restaurants) {
         if (order.restaurant.name === restaurant.name) {
           restaurantId = restaurant._id;
+          rating = restaurant.rating;
           for (let i = 0; i < restaurant.menu.length; i++) {
             restaurantMenu.push(restaurant.menu[i])
           }
@@ -45,7 +46,6 @@ module.exports = async function (fastify, opts) {
       )
       restaurantMenu = [];
       currentOrderedMenu = [];
-      rating += 1
     }
 
     reply
@@ -66,20 +66,18 @@ module.exports = async function (fastify, opts) {
     let restaurantId;
     let restaurantMenu = [];
     let rating;
-    let index = 0;
     let currentOrderedMenu = [];
 
     for (let order of orders) {
       if (req.params.id === `${order._id}`) {
         currentOrder = order;
-        rating = index;
       }
-      index += 1;
     }
 
     for (let restaurant of restaurants) {
       if (currentOrder.restaurant.name === restaurant.name) {
         restaurantId = restaurant._id;
+        rating = restaurant.rating;
         for (let i = 0; i < restaurant.menu.length; i++) {
           restaurantMenu.push(restaurant.menu[i])
         }
@@ -109,5 +107,72 @@ module.exports = async function (fastify, opts) {
       .send(result)
 
   })
+
+  fastify.post('/', async function (req, reply) {
+    const courier = ['김배송', '이배송', '박배송', '권배송', '전배송']
+    const db = this.mongo.client.db('baedal');
+    const collectionOfOrders = db.collection('order');
+    const collectionOfrestaurants = db.collection('restaurants');
+    const restaurantid = this.mongo.ObjectId(req.body.restaurantId);
+    const restaurants = await collectionOfrestaurants.find({ "_id": restaurantid }).toArray();
+
+    const newOrderObjectId = this.mongo.ObjectId();
+    const newConsumerObjectId = this.mongo.ObjectId();
+    const currentOrdercourier = courier[Math.floor(Math.random() * 5)];
+    let restaurantMenu = [];
+    let orderedMenu = [];
+    let insertOrderMenu = [];
+
+    for (let menu of restaurants[0].menu) {
+      restaurantMenu.push(menu);
+    }
+
+    for (let menu of req.body.menu) {
+      orderedMenu.push(menu);
+      delete menu._id;
+      insertOrderMenu.push(menu)
+    }
+
+    await collectionOfOrders.insertOne(
+      {
+        "_id": newOrderObjectId,
+        "deliveryInfo": {
+          "status": "preparing",
+          "assignedCourier": currentOrdercourier,
+          "estimateDeleveryTime": 40
+        },
+        "consumer_id": newConsumerObjectId,
+        "restaurant": {
+          "name": restaurants[0].name,
+          "address": restaurants[0].address
+        },
+        "orderedMenu": insertOrderMenu
+      }
+    )
+
+    const result = {
+      "_id": newOrderObjectId,
+      "restaurant": {
+        "_id": restaurants[0]._id,
+        "name": restaurants[0].name,
+        "menu": restaurantMenu,
+        "address": restaurants[0].address,
+        "rating": restaurants[0].rating
+      },
+      "orderedMenu": orderedMenu,
+      "deliveryInfo": {
+        "status": "PREPARING",
+        "assignedCourier": "김배달",
+        "estimateDeleveryTime": 40
+      }
+    }
+
+    reply
+      .code(201)
+      .header('content-type', 'application/json')
+      .send(result)
+
+  })
+
 }
 
